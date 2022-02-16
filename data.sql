4324c6fcca6621b7adff6c8698d760dbd07b7d6c
Ahmed <ahmedreda152@gmail.com>
2022-02-16 18:41:47 +0200

    finish queries, data, and aggregations

--- data.sql
+++ data.sql
@@ -10,6 +10,7 @@ INSERT INTO
   )
 VALUES
   (0, 'Agumon', '2020/02/03', '0', 'true', '10.23');
+
 INSERT INTO
   animals (
     id,
@@ -21,6 +22,7 @@ INSERT INTO
   )
 VALUES
   (1, 'Gabumon', '2018/11/15', '2', 'true', '8');
+
 INSERT INTO
   animals (
     id,
@@ -39,6 +41,7 @@ VALUES
     'false',
     '15.04'
   );
+
 INSERT INTO
   animals (
     id,
@@ -50,3 +53,89 @@ INSERT INTO
   )
 VALUES
   (3, 'Devimon', '2017/05/12', '5', 'true', '11');
+
+INSERT INTO
+  animals (
+    id,
+    animal_name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES
+  (
+    4,
+    'Charmander',
+    '2020/02/08',
+    '0',
+    'false',
+    '-11'
+  );
+
+INSERT INTO
+  animals (
+    id,
+    animal_name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES
+  (5, 'Plantmon', '2022/11/15', '2', 'true', '-5.7');
+
+INSERT INTO
+  animals (
+    id,
+    animal_name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES
+  (
+    6,
+    'Squirtle',
+    '1993/04/02',
+    '3',
+    'false',
+    '-12.13'
+  );
+
+INSERT INTO
+  animals (
+    id,
+    animal_name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES
+  (7, 'Angemon', '2005/06/12', '1', 'true', '-45');
+
+INSERT INTO
+  animals (
+    id,
+    animal_name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES
+  (8, 'Boarmon', '2005/06/07', '7', 'true', '20.4');
+
+INSERT INTO
+  animals (
+    id,
+    animal_name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weight_kg
+  )
+VALUES
+  (9, 'Blossom', '1998/10/13', '3', 'true', '17');
