a89dfa6c51eb258de5fbdf8cdedb8e8e037cc7b5
Ahmed <ahmedreda152@gmail.com>
2022-02-18 13:56:58 +0200

    add vets,speciallization, and visits table

--- schema.sql
+++ schema.sql
@@ -47,3 +47,27 @@ ALTER TABLE
   animals
 ADD
   CONSTRAINT fk_animal_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets(
+  id SMALLSERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+  id SMALLSERIAL PRIMARY KEY,
+  vets_id SMALLSERIAL,
+  species_id SMALLSERIAL,
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  id SMALLSERIAL PRIMARY KEY,
+  animals_id SMALLSERIAL,
+  vets_id SMALLSERIAL,
+  date_of_visit DATE,
+  CONSTRAINT vet_fk FOREIGN KEY(vets_id) REFERENCES vets(id),
+  CONSTRAINT animals_fk FOREIGN KEY(animals_id) REFERENCES animals(id)
+);
